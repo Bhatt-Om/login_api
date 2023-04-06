9fab50707011370f3744c56b89a1d95bf7cc90c3
Bhatt-Om <bhattom1111@gmail.com>
2023-04-06 11:45:04 +0530

    Add devise gem, create devise user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "bookmarks#index"
+  devise_for :users
   resources :bookmarks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
